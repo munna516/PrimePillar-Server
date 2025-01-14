67b75377db3a6dd35b5a5dde215363609ee428ea
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-01-15 00:00:27 +0600

    jwt implemented done

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// Token Verify Midddlewares
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "Unauthorized access!!" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized access!!" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hqlh5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -24,6 +40,14 @@ async function run() {
     const DB = client.db("Prime-Pillar");
     const apartmentCollection = DB.collection("Apartments");
     const agreementCollection = DB.collection("Agreements");
+    // JWT API
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1200h",
+      });
+      res.send({ token });
+    });
     // Get all apartments
     app.get("/apartments", async (req, res) => {
       const result = await apartmentCollection.find().toArray();
