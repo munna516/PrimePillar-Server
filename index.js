2d9592c55f57f080117eeda73a5cdf13eebd268a
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-01-14 10:02:16 +0600

    all apartment data insert in DB and get it using API

--- index.js
+++ index.js
@@ -20,13 +20,22 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // Database
+    const DB = client.db("Prime-Pillar");
+    const apartmentCollection = DB.collection("Apartments");
+    // Get all apartments
+    app.get("/apartments", async (req, res) => {
+      const result = await apartmentCollection.find().toArray();
+      res.send(result);
+    });
+    // Connect MongoDB Client
     await client.connect();
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
